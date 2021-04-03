423540d870cc0ed50e9667844f7a8d15c9c22190
JellyBongo <superseriousbox@gmail.com>
2021-04-03 21:59:50 +0300

    Basic elevator's interface

---
+++ src/elevator.py
@@ -0,0 +1,46 @@
+class Elevator:
+    def __init__(self, tonnage: int, floors_count: int, cur_direction: int, 
+        cur_weight: int, is_light_on: bool, is_smoked: bool, requests: list,
+        is_communication_on: bool, is_doors_open: bool, 
+        is_empty: bool, current_floor: int):
+        pass
+
+    def open_doors(self):
+        pass
+
+    def close_doors(self):
+        pass
+
+    def turn_light_on(self):
+        pass
+
+    def turn_light_off(self):
+        pass
+
+    def alarm(self):
+        pass
+
+    def move_to_floor(self, floor: int):
+        pass
+
+    def add_request(self, request):
+        pass
+
+    def next_request(self):
+        pass
+
+    def turn_smoke_on(self):
+        pass
+
+    def turn_smoke_off(self):
+        pass
+
+    def is_door_blocked(self):
+        pass
+
+    def call_dispatcher(self):
+        pass
+
+    def trigger(self):
+        pass
+
