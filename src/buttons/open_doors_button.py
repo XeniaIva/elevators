c4dd507c2150230e4a9024fa697205edb3b369b7
JellyBongo <superseriousbox@gmail.com>
2021-04-05 21:44:21 +0300

    Basic interface for sensors and buttons

---
+++ src/buttons/open_doors_button.py
@@ -0,0 +1,10 @@
+class OpenDoorsButton:
+    def __init__(self, elevator: Elevator, operator: Operator):
+        pass
+
+    # Think about return type
+    def get_state(self):
+        pass
+
+    def trigger(self):
+        pass
---
+++ src/buttons/up_button.py
@@ -0,0 +1,10 @@
+class UpButton:
+    def __init__(self, elevator: Elevator, operator: Operator):
+        pass
+
+    # Think about return type
+    def get_state(self):
+        pass
+
+    def trigger(self):
+        pass
